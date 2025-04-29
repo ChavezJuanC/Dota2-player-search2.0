e64f3022b7fe6049a08740b20f959dfbfabd901f
JuanChavez <dosomethingwild012712@gmail.com>
2025-04-29 02:37:42 -0600

    Implemented reactive nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,86 @@
 import { Tabs } from "expo-router";
+import { Image, View, Text, ImageSourcePropType } from "react-native";
+import { icons } from "@/constants/icons";
+
+interface TabIconInterface {
+    label: string;
+    icon: ImageSourcePropType;
+    focused: boolean;
+}
+
+const TabIcon = ({ label, icon, focused }: TabIconInterface) => (
+    <View className="flex flex-col min-w-24 max-w-32 justify-center items-center pt-8">
+        <Image
+            source={icon}
+            className="size-8"
+            style={focused ? { tintColor: "blue" } : { tintColor: "#C1C0BF" }}
+        />
+        <Text className="text-navTabs">{label}</Text>
+    </View>
+);
 
 export default function TabLayout() {
     return (
-        <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: "100%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                },
+                tabBarStyle: {
+                    backgroundColor: "#1F1927",
+                },
+            }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
-                    title: "Home",
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            label="Home"
+                            icon={icons.home}
+                            focused={focused}
+                        />
+                    ),
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options={{
-                    title: "Profile",
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            label="Profile"
+                            icon={icons.profile}
+                            focused={focused}
+                        />
+                    ),
                 }}
             />
             <Tabs.Screen
                 name="games"
                 options={{
-                    title: "Games",
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            label="Games"
+                            icon={icons.games}
+                            focused={focused}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            label="Settings"
+                            icon={icons.settings}
+                            focused={focused}
+                        />
+                    ),
                 }}
             />
         </Tabs>
@@ -31,5 +93,3 @@ Nav [(Home)(Profile)(Match)(Settings)]
 (Profile):
 (Match Search): -> Match Results
 */
-
-
---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import home from "@/assets/icons/home.png";
+import profile from "@/assets/icons/user.png";
+import games from "@/assets/icons/keyboard-and-mouse.png";
+import settings from "@/assets/icons/setting-lines.png";
+
+export const icons = {
+    home,
+    profile,
+    games,
+    settings,
+};
